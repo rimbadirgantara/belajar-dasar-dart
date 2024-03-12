ddd5631bc2cd128249d06d5d50e6a9845d897af7
rimbadirgantara <mcsegypt17@gmail.com>
2024-03-12 17:23:08 +0700

    belajar kondisi (if - else)

---
+++ if-else/run.dart
@@ -0,0 +1,27 @@
+void main() {
+  /* 
+  if (boolean expression) {
+    jika boolean menghasilkan true
+  } else {
+    jika boolean menghasilkan false
+  }
+  */
+
+  int angka = 4;
+  if (angka == 0) {
+    print('Nol');
+  } else {
+    if (angka % 2 == 0) {
+    print('Angka Genap');
+  } else {
+    print('Angka Ganjil');
+  }
+  }
+
+  String nama = 'rimba';
+  if (nama == 'rimba'){ 
+    print('ini rimba');
+  } else {
+    print('bukan rimba');
+  }
+}
