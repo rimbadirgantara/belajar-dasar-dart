fd981041371f8e340af89756f82f93dee9d3984d
rimbadirgantara <mcsegypt17@gmail.com>
2024-03-12 17:29:09 +0700

    belajar switch case

---
+++ switch-case/run.dart
@@ -0,0 +1,33 @@
+void main() {
+  /* 
+  switch(variabel expression) {
+    case value:
+      // stetamen
+    break;
+    
+    case value:
+      // stetamen
+
+    break;
+
+    default:
+      // stetamen
+    break;
+  }
+  */  
+
+  var nilai = 'A';
+  switch(nilai) {
+    case 'A':
+      print('sangat baik');
+      break;
+    case 'B':
+      print('baik');
+      break;  
+    case 'C':
+      print('kurang baik');
+      break;
+    default:
+      print('tidak valid');
+  }
+}
